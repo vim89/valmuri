a1258d65a6b40daa2ebfb2e957fcdfd5050dc6d3
Vitthal Mirji <vitthalmirji@gmail.com>
2025-07-26 08:43:36 +0530

    DI/Config implementation: DRAFT

--- src/main/scala/valmuri/http/HttpAppAdapter.scala
+++ src/main/scala/valmuri/http/HttpAppAdapter.scala
@@ -1,19 +1,18 @@
-// src/main/scala/valmuri/http/HttpAppAdapter.scala
 package valmuri.http
 
-import valmuri.routing.{Router, Request => ValmuriRequest, Response => ValmuriResponse}
+import valmuri.routing.{ Request => ValmuriRequest, Response => ValmuriResponse, Router }
 import zio._
 import zio.http._
 import zio.prelude.data.Optional.AllValuesAreNullable
 
 object HttpAppAdapter {
-  def fromRouter(router: Router): Routes[Any, Response] = {
+  def fromRouter(router: Router): Routes[Any, Response] =
     Routes(
       Method.ANY / trailing -> handler { (req: Request) =>
         val valmuriRequest = ValmuriRequest(
           method = req.method.toString,
           path = req.url.path.toString,
-          params = Map.empty // Path params will be extracted by router
+          params = Map.empty, // Path params will be extracted by router
         )
 
         val valmuriResponse = router.route(valmuriRequest)
@@ -24,10 +23,9 @@ object HttpAppAdapter {
           Response(
             status = status,
             headers = Headers(Header.ContentType(MediaType.application.json)),
-            body = Body.fromString(valmuriResponse.body)
+            body = Body.fromString(valmuriResponse.body),
           )
         )
       }
     )
-  }
 }
